f5d00aa10d4883c6d26c81498d2297996bcefbe6
pranjalipongde09 <pranjalipongde9@gmail.com>
2025-11-20 17:26:58 +0530

    added some animations

--- src/components/sidebar/SidebarProfile.jsx
+++ src/components/sidebar/SidebarProfile.jsx
@@ -9,9 +9,13 @@ import ContactDivider from "./ContactDivider";
 const SidebarProfile = () => {
   return (
     <motion.aside
-      initial={{ opacity: 0, x: -20 }}
+      initial={{ opacity: 0, x: -80 }}
       animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.5 }}
+      transition={{
+        type: "spring",
+        stiffness: 60,
+        damping: 14,
+      }}
       className="md:sticky md:top-10 h-fit space-y-6 border border-white/10 p-6 rounded-xl bg-white/5"
     >
       {/* Profile Section */}
